9a290128d3d85c8f6198d634b3ba4404536e1a6e
tomascabralh <tomascabralh@gmail.com>
2020-11-14 16:44:08 -0300

    Nuevos cambios

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react'
-
+import styles from './Layout.module.css'
 import Auxiliary from '../../hoc/Auxiliary'
 
 const layout = (props) => (
     <Auxiliary>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
-        {props.children}
-    </main>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={styles.Content}>
+            {props.children}
+        </main>
     </Auxiliary>
 )
 
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Auxiliary from '../../hoc/Auxiliary'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+class BurguerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+    render() {
+        return (
+            <Auxiliary>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </Auxiliary>
+        )
+    }
+}
+
+export default BurguerBuilder;

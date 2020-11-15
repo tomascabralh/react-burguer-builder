82b48dfc6523be77a25bc29e8e0020ac9854cefd
Tomas Cabral Hunter <tomascabralh@gmail.com>
2020-11-15 17:49:26 -0300

    agrego logica

---
+++ src/containers/BurguerBuilder/BurgerBuilder.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import Auxiliary from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  meat: 3,
+  cheese: 1,
+  bacon: 2,
+};
+
+
+class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0,
+    },
+    totalPrice: 20
+  };
+
+  addIngredientHandler = ( type ) => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount + 1;
+    const updatedIngredients = {
+        ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceAddition = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice + priceAddition;
+    this.setState( { totalPrice: newPrice, ingredients: updatedIngredients } );
+}
+
+removeIngredientHandler = ( type ) => {
+}
+
+  render() {
+    return (
+      <Auxiliary>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls 
+        ingredientAdded={this.addIngredientHandler}/>
+      </Auxiliary>
+    );
+  }
+}
+
+export default BurgerBuilder;
